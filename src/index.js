4c979068a3d81ea9b67f8659d8a9bd0b281e350e
Abhi Bhatti <abhibhatti1546@gamil.com>
2026-08-09 01:11:33 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./.env",
+  debug :true
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+app.listen(process.env.PORT || 8000 , ()=>{
+   console.log(`server is running  at port : ${process.env.PORT}`)
+})
+
+})
+.catch((err)=>{
+  console.log("Mango Db connection failed !!!" , err);
+
+})
 
 
 
